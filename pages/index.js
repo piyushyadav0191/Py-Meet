eb426fc8aa3444519254dafc67148af0ec5ae9d3
piyushyadav0191 <piyushyadav0191@gmail.com>
2021-12-11 20:01:48 +0530

    ready for commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
+import { Fragment } from 'react';
+
+
+function HomePage (props) {
+  return (
+  <Fragment>
+   <Head> 
+     <title>Py Meet</title>
+     <meta name="description" content="Browse your favourite city " />
+   </Head>
+   <MeetupList meetups={ props.meetups } /> ;
+  </Fragment>
+  
+  );
+        
+}
+export async function getStaticProps() {
+
+    const client = await MongoClient.connect('mongodb+srv://piyushyadav0191:yadav123456@cluster0.0aaiv.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
+ 
+      const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+      client.close();
+
+   return {
+props: {
+    meetups: meetups.map(meetup => ({
+        title: meetup.data.title,
+        address: meetup.data.address,
+        image: meetup.data.image ,
+        id: meetup._id.toString(),
+    })),
+},
+  revalidate: 1,
+   };
+}
+
+
+export default HomePage;
